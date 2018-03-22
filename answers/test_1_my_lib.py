69289d2b7fb9d8df9cb7356c9147c49dd495e9a3
Matt Williams <matt@milliams.com>
2018-03-22 11:54:55 +0000

    Initial commit

---
+++ answers/test_1_my_lib.py
@@ -0,0 +1,5 @@
+from my_lib import add_elements
+
+def test_add():
+    assert add_elements([1, 2], [3, 4]) == [4, 7]
+
---
+++ my_lib.py
@@ -0,0 +1,6 @@
+def add_elements(a, b):
+    z = []
+    for i in range(len(a)):
+        z.append(a[i] + b[i])
+    return z
+
